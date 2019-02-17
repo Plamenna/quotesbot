4c5eedd77374af527fc50de6febdaedbe73a9044
Plamenna <plamennavenkova@gmail.com>
2019-02-17 11:29:29 +0100

    improve it

--- quotesbot/spiders/make_db/create_new_db.py
+++ quotesbot/spiders/make_db/create_new_db.py
@@ -1,22 +1,49 @@
 import sqlite3
 
-sqlite_file = 'create_newdb.db' 
-table_name1 = 'Jobs_Title'  
-#table_name2 = 'Jobs_Links'  
+
+def insertValuesInDb(value1,value2):
+    sqlite_file = 'create_newdb.db' 
+    table_name1 = 'Jobs_Title'  
+    table_name2 = 'Jobs_Links'  
 
 
-new_field='Title_Of_the_Job'
-field_type='TEXT'
+    new_field='Title_Of_the_Job'
+    new_field2='Links'
+    field_type='TEXT'
 
 
-conn = sqlite3.connect('create_newdb.db')
-c = conn.cursor()
 
 
-c.execute('CREATE TABLE {tn} ({nf} {ft})'.format(tn=table_name1, nf=new_field, ft=field_type))
 
 
-conn.commit()
-conn.close()
+    conn = sqlite3.connect('/home/rado/quotesbot/quotesbot/spiders/make_db/create_newdb.db')
+    c = conn.cursor()
+    #delete the tables
+    #c.execute('DROP TABLE Jobs_Links')
+    #c.execute('DROP TABLE Jobs_Title')
 
+    #add column to an existing table
+    #c.execute('ALTER TABLE {table_name} ADD COLLUMN {type1}'.format(table_name=table_name1,type1='TEXT'))
+
+
+
+    
+    #create table with two columns
+    #c.execute('CREATE TABLE IF NOT EXISTS {tn} ({nf} {ft}, {af} {bt}) '.format(tn=table_name1, nf=new_field, ft=field_type, af=new_field2,bt=field_type))
+
+
+
+
+    #inserting some values into the table
+    c.execute('INSERT INTO {what} ({c1}, {c2}) VALUES ({v1}, {v2})'.format(what=table_name1, c1=new_field,c2=new_field2,v1=value1,v2=value2))
+    
+    
+    #c.execute('CREATE TABLE IF NOT EXISTS {tn} ({nf} {ft} PRIMARY KEY)'.format(tn=table_name2, nf=new_field2, ft=field_type))
+    #c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}".format(tn=table_name, cn=new_column1, ct=column_type))
+
+
+
+    conn.commit()
+    conn.close()
+    return
 
--- quotesbot/spiders/toscrape-css.py
+++ quotesbot/spiders/toscrape-css.py
@@ -1,28 +1,19 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
-
+import create_new_db 
 class ToScrapeCSSSpider(scrapy.Spider):
     name = "toscrape-css"
-    start_urls = [
-        #'http://quotes.toscrape.com/'
-        'https://www.jobs.ch/en/vacancies/?location=geneva&term='
-    ]
+    start_urls = ['https://www.jobs.ch/en/vacancies/?location=geneva&term=']
 
     def parse(self, response):
-        jobs={}
-        key='Java'
+        key='Controler'
         for quote in response.css("a.x--job-link.t--job-link"):
-
-                yield {
-                        'Job Title': quote.css("a.x--job-link.t--job-link::text").extract(),
-                        'Link to the Job': 'https://www.jobs.ch'+quote.css("a.x--job-link.t--job-link::attr(href)").get()
-
-                    #.attrib["href"]
-                    
-                }
-
-
+            if 'Controler' in str(quote.css("a.x--job-link.t--job-link::text").extract()):
+                #value1=str(quote.css("a.x--job-link.t--job-link::text").extract())
+                #value2='https://www.jobs.ch'+quote.css("a.x--job-link.t--job-link::attr(href)").get()
+                value1=100
+                value2=300
+                create_new_db.insertValuesInDb(value1,value2)
 
         next_page_url = response.css("li.next > a::attr(href)").extract_first()
         if next_page_url is not None:
